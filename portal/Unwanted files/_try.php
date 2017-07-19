0d6bc18f9671e469a50722ac8056764f97e000fb
Rahul Patil <rahulpatil61197@gmail.com>
2017-07-19 20:14:17 +0530

    Deleted some files

--- portal/Unwanted files/_try.php
+++
@@ -1,6 +0,0 @@
-<!DOCTYPE html>
-<html>
-<body>
-	<a href="_try2.php"><button id="click" name="click" value="Delete">Delete Contents</button></a>
-</body>
-</html>
--- portal/Unwanted files/_try4.php
+++
@@ -1,7 +0,0 @@
-<?php
-	$name = $_POST['name'];
-	$city = $_POST['city'];
-	$id = $_POST['id'];
-	//$id = 1;
-	echo $id . " " . $name . " lives in " . $city;
-?>
